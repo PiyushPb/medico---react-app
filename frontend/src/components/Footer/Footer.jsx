39bec8609ec1ac890fcc41bb9ba1dc0725bd7a50
Piyush Pardeshi <piyushp0512@gmail.com>
2023-11-25 22:40:07 +0530

    home page completed

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,7 +1,108 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../assets/images/logo.png";
+import { RiLinkedinFill } from "react-icons/ri";
+import {
+  AiFillYoutube,
+  AiFillGithub,
+  AiOutlineInstagram,
+} from "react-icons/ai";
+
+const socialLinks = [
+  {
+    path: "https://www.linkedin.com/in/piyushpardeshi/",
+    icon: <RiLinkedinFill className="group-hover:text-white w-4 h-5" />,
+  },
+  {
+    path: "https://github.com/PiyushPb",
+    icon: <AiFillGithub className="group-hover:text-white w-4 h-5" />,
+  },
+  {
+    path: "https://www.instagram.com/codewithpiyush/",
+    icon: <AiOutlineInstagram className="group-hover:text-white w-4 h-5" />,
+  },
+  {
+    path: "https://www.youtube.com/@codewithpiyush",
+    icon: <AiFillYoutube className="group-hover:text-white w-4 h-5" />,
+  },
+];
+
+const quickLinks01 = [
+  {
+    path: "/",
+    display: "Home",
+  },
+  {
+    path: "/about",
+    display: "About Us",
+  },
+  {
+    path: "/services",
+    display: "Services",
+  },
+  {
+    path: "/contact",
+    display: "Contact Us",
+  },
+];
+
+const quickLinks02 = [
+  {
+    path: "/",
+    display: "Prathamesh Sonwane",
+  },
+  {
+    path: "/",
+    display: "Sanket",
+  },
+  {
+    path: "/",
+    display: "Rohan",
+  },
+];
+
+const quickLinks03 = [
+  {
+    path: "/",
+    display: "Privacy Policy",
+  },
+  {
+    path: "/",
+    display: "Terms & Conditions",
+  },
+  {
+    path: "/",
+    display: "FAQ",
+  },
+];
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const year = new Date().getFullYear();
+  return (
+    <footer className="pb-16 pt-10">
+      <div className="container">
+        <div className="flex justify-between flex-col md:flex-row flex-wrap gap-[30px]">
+          <div>
+            <img src={Logo} alt="" />
+            <p className="text-[16px] leading-7 font-[400] text-textColor mt-4">
+              Copyright © {year} developed by Prathamesh and group
+            </p>
+            <div className="flex items-center gap-3 mt-4">
+              {socialLinks.map((item, index) => (
+                <Link
+                  to={item.path}
+                  key={index}
+                  className="w-9 h-9 border border-solid border-[#181a1e] rounded-full flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+                >
+                  {item.icon}
+                </Link>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
