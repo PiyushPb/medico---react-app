05bccdc7d16b2fad3fd65d41f1fed491e72242d1
Piyush Pardeshi <piyushp0512@gmail.com>
2023-11-28 20:46:46 +0530

    signup page added

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-export default Login
+  const handleInputChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  return (
+    <div className="w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10">
+      <h3 className="text-headingColor text-[22px] leading-9 font-bold mb-10">
+        Hello <span className="text-primaryColor">Welcome</span> Back 👋👋
+      </h3>
+      <form className="py-4 md:py-0">
+        <div className="mb-5">
+          <input
+            type="email"
+            placeholder="someone@google.com"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            className="w-full py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer"
+            required
+          />
+        </div>
+        <div className="mb-5">
+          <input
+            type="password"
+            placeholder="Type your password here!"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+            className="w-full py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer"
+            required
+          />
+        </div>
+        <div className="mt-0">
+          <button className="btn w-full rounded-lg">Login</button>
+        </div>
+        <p className="mt-5 text-textColor text-center">
+          Don&apos;t have an account?{" "}
+          <Link to="/signup" className="text-primaryColor font-medium ml-1">
+            Register
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,123 @@
 import React from "react";
+import signupImg from "../assets/images/signup.gif";
+import avatar from "../assets/images/avatar-icon.png";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  return (
+    <section className="px-5 xl:px-0">
+      <div className="max-w-[1170px] mx-auto">
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+          <div className="hidden lg:block bg-primaryColor rounded-l-lg">
+            <figure className="rounded-l-lg">
+              <img src={signupImg} alt="" className="w-full rounded-lg" />
+            </figure>
+          </div>
+
+          {/* Signup form */}
+
+          <div className="rounded-l-lg lg:pl-16 py-10">
+            <h3 className="text-headingColor text-[22px] font-bold mb-10">
+              Create an <span className="text-primaryColor">account</span>
+            </h3>
+            <form>
+              <div className="mb-5">
+                <input
+                  type="text"
+                  placeholder="Full name"
+                  name="name"
+                  className="w-full pr-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer"
+                  required
+                />
+              </div>
+              <div className="mb-5">
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  name="email"
+                  className="w-full pr-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer"
+                  required
+                />
+              </div>
+              <div className="mb-5">
+                <input
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  className="w-full pr-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor cursor-pointer"
+                  required
+                />
+              </div>
+
+              <div className="mb-5 flex items-center justify-between">
+                <label
+                  htmlFor=""
+                  className="text-headingColor font-bold text-[16px] leading-7"
+                >
+                  Are you a:
+                  <select
+                    name="role"
+                    className="text-textColor font-semiBold text-[15px] leading-7 px-4 py-3 focus:outline-none"
+                  >
+                    <option value="patient">Patient</option>
+                    <option value="doctor">Doctor</option>
+                  </select>
+                </label>
+                <label
+                  htmlFor=""
+                  className="text-headingColor font-bold text-[16px] leading-7"
+                >
+                  Gender :
+                  <select
+                    name="gender"
+                    className="text-textColor font-semiBold text-[15px] leading-7 px-4 py-3 focus:outline-none"
+                  >
+                    <option value="patient">Male</option>
+                    <option value="doctor">Female</option>
+                    <option value="other">Female</option>
+                  </select>
+                </label>
+              </div>
+              <div className="mb-5 flex items-center gap-3">
+                <figure className="w-[60px] h-[60px] rounded-full border-2 border-solid border-primaryColor flex items-center justify-center ">
+                  <img
+                    src={avatar}
+                    alt="..."
+                    className="w-full h-full rounded-full"
+                  />
+                </figure>
+
+                <div className="relative w-[160px] h-[50px]">
+                  <input
+                    type="file"
+                    name="photo"
+                    id="customFile"
+                    accept=".jpg, .jpeg, .png"
+                    className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer"
+                  />
+                  <label
+                    htmlFor="customFile"
+                    className="absolute top-0 left-0 w-full h-full flex items-center px-[0.75rem] py-[0.375rem] text-[15px] leading-6 overflow-hidden bg-[#0066ff46] text-headingColor rounded-lg font-semibold truncate cursor-pointer"
+                  >
+                    Upload a photo
+                  </label>
+                </div>
+              </div>
+              <p className="mt-5 text-textColor text-center">
+                Allready have an account?
+                <Link
+                  to="/Login"
+                  className="text-primaryColor font-medium ml-1"
+                >
+                  Login
+                </Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Signup;
